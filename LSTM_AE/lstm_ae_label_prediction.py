9f92edd635b8ff6b44a9c3720e58f303c0df01a5
eilondu <eilonduchan@gmail.com>
2024-03-04 09:22:09 +0200

    corrected the model and data sent to it

--- LSTM_AE/lstm_ae_label_prediction.py
+++ LSTM_AE/lstm_ae_label_prediction.py
@@ -1,16 +1,17 @@
 import argparse
+import numpy as np
 import pandas as pd
 from keras import Input
 from keras.optimizers import Adam
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import KFold
-from data_visualization import df_to_sequence_input
+# from data_visualization import df_to_sequence_input
 from keras.layers import LSTM, TimeDistributed, Dense, RepeatVector
 from keras.models import Model
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=1000)
+parser.add_argument('--epochs', type=int, default=100)
 parser.add_argument('--optimizer', type=str, default='Adam')
 parser.add_argument('--learning_rate', type=float, default=1e-3)
 parser.add_argument('--gradient_clipping', type=float, default=1.0)
@@ -19,6 +20,19 @@ parser.add_argument('--input_size', type=int, default=2)
 parser.add_argument('--hidden_state_size', type=int, default=64)
 args = parser.parse_args()
 
+
+def df_to_sequence_input(df, time_steps):
+    # print(df.shape)
+    print(f'df len is{len(df)}')
+    subsequences = []
+    sub_seq_labels = []
+    for i in range(len(df) - time_steps):
+        row = df[i: i + time_steps]
+        subsequences.append(row)
+        label = df[i + time_steps]
+        sub_seq_labels.append(label)
+    # print(np.array(subsequences).shape)
+    return np.array(subsequences), np.array(sub_seq_labels)
 
 def make_model(hidden_state_dims, time_steps, data_features):
     input_layer = Input(shape=(time_steps, data_features))
@@ -32,6 +46,77 @@ def make_model(hidden_state_dims, time_steps, data_features):
     return model
 
 
+def prepare_data_by_symbol_multi_step(df, N):
+
+    X, Y, symbols = [], [], []
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    # Ensure 'date' is a datetime column
+    df['date'] = pd.to_datetime(df['date'])
+
+    # Create separate year and month columns for grouping
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month
+
+    # Group by symbol, year, and month, then take the first entry of each group
+    df_monthly = df.groupby(['symbol', 'year', 'month']).first().reset_index()
+
+    # Make sure 'date' is not duplicated in df_monthly
+    if 'date' in df_monthly.columns:
+        df_monthly = df_monthly.drop(columns=['date'])
+
+    df_monthly = df_monthly[['symbol', 'high']]
+    # df=df[df['symbol'] == 'AMZN']
+    df_monthly = df_monthly.dropna()
+
+    # Iterate over each symbol
+    for symbol in df_monthly['symbol'].unique():
+        symbol_df = df_monthly[df_monthly['symbol'] == symbol]
+        scaled_data = scaler.fit_transform(symbol_df[['high']])
+        # print(scaled_data)
+        if(len(scaled_data) == 48): # take only stock with all the data
+            # Create sequences for each symbol
+            for i in range(0, len(symbol_df) -1 - N):
+                X.append(scaled_data[i :i + N])  # Assuming 'high' is the only feature we're interested in
+                Y.append(scaled_data[i + 1 + N])  # Next day's 'high' value as label
+                symbols.append(symbol)  # Keep track of the symbol for each sequence
+
+    X, Y = np.array(X), np.array(Y)
+    return X, Y, symbols
+
+def prepare_data_by_symbol(df):
+
+    X, symbols = [], []
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    # Ensure 'date' is a datetime column
+    df['date'] = pd.to_datetime(df['date'])
+
+    # Create separate year and month columns for grouping
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month
+
+    # Group by symbol, year, and month, then take the first entry of each group
+    df_monthly = df.groupby(['symbol', 'year', 'month']).first().reset_index()
+
+    # Make sure 'date' is not duplicated in df_monthly
+    if 'date' in df_monthly.columns:
+        df_monthly = df_monthly.drop(columns=['date'])
+
+    df_monthly = df_monthly[['symbol', 'high']]
+    # df=df[df['symbol'] == 'AMZN']
+    df_monthly = df_monthly.dropna()
+
+    # Iterate over each symbol
+    for symbol in df_monthly['symbol'].unique():
+        symbol_df = df_monthly[df_monthly['symbol'] == symbol]
+        scaled_data = scaler.fit_transform(symbol_df[['high']])
+        if(len(scaled_data[['high']]) == 48): # take only stock with all the data
+            X.append(scaled_data[['high']])
+            symbols.append(symbol)
+
+    return np.array(X), symbols
+
 def create_and_train_lstmae_predictor(time_steps):
     optimizer_class = {
         'Adam': Adam,
@@ -39,25 +124,21 @@ def create_and_train_lstmae_predictor(time_steps):
 
     # Load the S&P500 stock price dataset
     df = pd.read_csv('SP 500 Stock Prices 2014-2017.csv')
-    df_dropped = df.dropna()
-    df_filtered = df_dropped[df_dropped['symbol'].isin(['AMZN'])]
-    # normalize data
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    high_data = scaler.fit_transform(df_filtered[['high']].values)
+    first_day_each_month, Y, symbols = prepare_data_by_symbol_multi_step(df, time_steps)
+    kf = KFold(n_splits=2, shuffle=True, random_state=42)
+    # grouped = first_day_each_month.groupby('symbol')
+    # # print(grouped)
 
-    kf = KFold(n_splits=5, shuffle=True, random_state=42)
-
-    # Initialize variables to track the best model
+    # Cross-validation loop
     best_model = None
     best_loss = float('inf')
     best_history = None
-    data_features = high_data.shape[1]
-
-    # Cross-validation loop
-    for train_index, test_index in kf.split(high_data):
+    data_features = first_day_each_month[0].shape[1]
+    # print(f'high data shape:{high_data.shape}')
+    for train_index, test_index in kf.split(first_day_each_month):
         # Split data into train and test sets
-        X, Y = df_to_sequence_input(high_data[train_index], time_steps)
-        X_test, labels = df_to_sequence_input(high_data[test_index], time_steps)
+        X_train, Y_train = first_day_each_month[train_index], Y[train_index]
+        X_test, labels = first_day_each_month[test_index], Y[test_index]
 
         # Initialize the model
         autoencoder = make_model(hidden_state_dims=args.hidden_state_size, time_steps=time_steps,
@@ -72,12 +153,7 @@ def create_and_train_lstmae_predictor(time_steps):
             loss=['mean_squared_error', 'mean_squared_error'],
             )
         # Train the model
-
-        history = autoencoder.fit(X, [X, Y], epochs=args.epochs, batch_size=args.batch_size)
-        # for output in autoencoder.outputs:
-        #     print(output.name)
-        # print(f'metric names:{autoencoder.metrics_names}')
-
+        history = autoencoder.fit(X_train, [X_train, Y_train], epochs=args.epochs, batch_size=args.batch_size)
         # Evaluate the model
         loss = autoencoder.evaluate(X_test, [X_test, labels])
         if loss[0] < best_loss:
@@ -92,7 +168,7 @@ def create_and_train_lstmae_predictor(time_steps):
 
 if __name__ == "__main__":
     # print(best_history.history.keys())
-    best_model, best_history = create_and_train_lstmae_predictor(time_steps=5)
+    best_model, best_history = create_and_train_lstmae_predictor(time_steps=1)
 
     plt.figure(figsize=(14, 7))
     plt.plot(best_history.history['reconstruction_loss'], label='Reconstruction Loss')
--- LSTM_AE/multi_step_prediction.py
+++ LSTM_AE/multi_step_prediction.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
@@ -6,66 +7,117 @@ from data_visualization import df_to_sequence_input
 from lstm_ae_label_prediction import create_and_train_lstmae_predictor
 
 
-df = pd.read_csv('SP 500 Stock Prices 2014-2017.csv')
-df_dropped = df.dropna()
-df_filtered = df_dropped[df_dropped['symbol'].isin(['AMZN'])]
-
 scaler = MinMaxScaler(feature_range=(0, 1))
-high_data = scaler.fit_transform(df_filtered[['high']].values)
+
+def prepare_data_seq(df_monthly, N):
+
+    X, Y, symbols = [], [], []
+
+    # Iterate over each symbol
+    for symbol in df_monthly['symbol'].unique():
+        symbol_df = df_monthly[df_monthly['symbol'] == symbol]
+        scaled_data = scaler.fit_transform(symbol_df[['high']])
+        # print(scaled_data)
+        if(len(scaled_data) == 48): # take only stock with all the data
+            # Create sequences for each symbol
+            for i in range(0, len(symbol_df) - 1 - N):
+                X.append(scaled_data[i :i + N])  # Assuming 'high' is the only feature we're interested in
+                Y.append(scaled_data[i + 1 + N])  # Next day's 'high' value as label
+                symbols.append(symbol)  # Keep track of the symbol for each sequence
+
+    X, Y = np.array(X), np.array(Y)
+    return X, Y, symbols
 
 
-multi_steps_model, _ = create_and_train_lstmae_predictor(time_steps=5)
-single_step_model, _ = create_and_train_lstmae_predictor(time_steps=1)
+if __name__ == "__main__":
+    # Ensure 'date' is a datetime column
+    df = pd.read_csv('SP 500 Stock Prices 2014-2017.csv')
 
-multi_step_sequences, _ = df_to_sequence_input(high_data,time_steps=5)
-single_step_sequences, _ = df_to_sequence_input(high_data,time_steps=1)
+    # print(len(df_filtered))
 
-_, reconstructed_multi_step_data = multi_steps_model.predict(multi_step_sequences)
-_, reconstructed_single_step_data = multi_steps_model.predict(single_step_sequences)
+    # high_data = scaler.fit_transform(df_filtered[['high']].values)
 
-reconstructed_multi_step_data = reconstructed_multi_step_data.reshape(-1, 1)
-reconstructed_prices_multi_step = scaler.inverse_transform(reconstructed_multi_step_data).flatten()
+    df['date'] = pd.to_datetime(df['date'])
 
-reconstructed_single_step_data = reconstructed_single_step_data.reshape(-1, 1)
-reconstructed_prices_single_step = scaler.inverse_transform(reconstructed_single_step_data).flatten()
+    # Create separate year and month columns for grouping
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month
 
-actual_prices = df_filtered['high'].values.reshape(-1, 1)[5:]
+    # Group by symbol, year, and month, then take the first entry of each group
+    df_monthly = df.groupby(['symbol', 'year', 'month']).first().reset_index()
 
-fig, ax = plt.subplots()
+    # Make sure 'date' is not duplicated in df_monthly
+    if 'date' in df_monthly.columns:
+        df_monthly = df_monthly.drop(columns=['date'])
 
-ax.plot(reconstructed_prices_multi_step, label='Reconstructed High Prices')
-ax.plot(actual_prices, label='Actual High Prices')
-ax.set_title(f'AMZ stock- Actual vs Predicted High Prices for Multi step')
-ax.set_xlabel('Time Step')
-ax.set_ylabel('High Prices')
-ax.grid(True)
-ax.legend()
+    df_monthly = df_monthly[['symbol', 'high']]
+    # df=df[df['symbol'] == 'AMZN']
+    df_monthly = df_monthly.dropna()
+    df_monthly = df_monthly[df_monthly['symbol'] == 'AMZN']
 
+    multi_steps_model, _ = create_and_train_lstmae_predictor(time_steps=24)
+    single_step_model, _ = create_and_train_lstmae_predictor(time_steps=1)
 
-ax_inset = fig.add_axes([0.2, 0.47, 0.25, 0.25])  # Position for the inset
-ax_inset.set_xlim(250, 300)
-ax_inset.set_ylim(350, 400)
-ax_inset.plot(reconstructed_prices_multi_step)
-ax_inset.plot(actual_prices)
-ax_inset.set_title('Magnified', fontsize=8)  # Smaller title font for the inset
-# ax_inset.tick_params(labelleft=False, labelbottom=False)  # Remove tick labels
-ax_inset.tick_params(labelbottom=False)  # Remove tick labels
-ax_inset.grid(True)
+    multi_step_sequences, _, _ = prepare_data_seq(df_monthly, N=24)
+    single_step_sequences, _, _ = prepare_data_seq(df_monthly, N=1)
 
-# Draw a rectangle on the main plot to indicate the magnified area
-rect = Rectangle((250, 350), 50, 50, edgecolor='red', facecolor='none', lw=2)
-ax.add_patch(rect)
-plt.show()
+    _, reconstructed_multi_step_data = multi_steps_model.predict(multi_step_sequences)
+    _, reconstructed_single_step_data = multi_steps_model.predict(single_step_sequences)
 
+    reconstructed_multi_step_data = reconstructed_multi_step_data.reshape(-1, 1)
+    reconstructed_prices_multi_step = scaler.inverse_transform(reconstructed_multi_step_data).flatten()
 
-plt.figure(figsize=(14, 7))
-plt.plot(reconstructed_prices_single_step, label='Reconstructed High Prices')
-plt.plot(actual_prices, label='Actual High Prices')
-plt.title(f'AMZ stock- Actual vs Predicted High Prices for Single step')
-plt.xlabel('Time Step')
-plt.ylabel('High Prices')
-plt.legend()
-plt.grid(True)
-plt.show()
+    reconstructed_single_step_data = reconstructed_single_step_data.reshape(-1, 1)
+    reconstructed_prices_single_step = scaler.inverse_transform(reconstructed_single_step_data).flatten()
+
+    actual_prices = df_monthly['high'].values
+
+    # fig, ax = plt.subplots()
+    #
+    # ax.plot(reconstructed_prices_multi_step, label='Reconstructed High Prices')
+    # ax.plot(actual_prices, label='Actual High Prices')
+    # ax.set_title(f'AMZ stock- Actual vs Predicted High Prices for Multi step')
+    # ax.set_xlabel('Time Step')
+    # ax.set_ylabel('High Prices')
+    # ax.grid(True)
+    # ax.legend()
+    #
+    #
+    # ax_inset = fig.add_axes([0.2, 0.47, 0.25, 0.25])  # Position for the inset
+    # ax_inset.set_xlim(250, 300)
+    # ax_inset.set_ylim(350, 400)
+    # ax_inset.plot(reconstructed_prices_multi_step)
+    # ax_inset.plot(actual_prices)
+    # ax_inset.set_title('Magnified', fontsize=8)  # Smaller title font for the inset
+    # # ax_inset.tick_params(labelleft=False, labelbottom=False)  # Remove tick labels
+    # ax_inset.tick_params(labelbottom=False)  # Remove tick labels
+    # ax_inset.grid(True)
+    #
+    # # Draw a rectangle on the main plot to indicate the magnified area
+    # rect = Rectangle((250, 350), 50, 50, edgecolor='red', facecolor='none', lw=2)
+    # ax.add_patch(rect)
+    # plt.show()
+
+    mid = len(actual_prices) // 2
+    actual_prices_ = actual_prices[mid+1:]
+    plt.figure(figsize=(14, 7))
+    plt.plot(reconstructed_prices_multi_step, label='Reconstructed High Prices')
+    plt.plot(actual_prices_, label='Actual High Prices')
+    plt.title(f'AMZ stock- Actual vs Predicted High Prices for Single step')
+    plt.xlabel('Time Step')
+    plt.ylabel('High Prices')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(figsize=(14, 7))
+    plt.plot(reconstructed_prices_single_step, label='Reconstructed High Prices')
+    plt.plot(actual_prices, label='Actual High Prices')
+    plt.title(f'AMZ stock- Actual vs Predicted High Prices for Single step')
+    plt.xlabel('Time Step')
+    plt.ylabel('High Prices')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
